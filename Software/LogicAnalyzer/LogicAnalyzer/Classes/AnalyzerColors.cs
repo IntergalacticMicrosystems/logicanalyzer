c9d23d2807ea01ce789aae36b4d7d8064e89b12f
Intergalactic Microsystems <matt@intergalacticmicrosystems.com>
2024-06-03 18:43:32 -0400

    Add more FgChannelColors for Multidevice

--- Software/LogicAnalyzer/LogicAnalyzer/Classes/AnalyzerColors.cs
+++ Software/LogicAnalyzer/LogicAnalyzer/Classes/AnalyzerColors.cs
@@ -46,6 +46,127 @@ namespace LogicAnalyzer.Classes
             Color.FromRgb(1, 255, 193),
             Color.FromRgb(63, 0, 255),
             Color.FromRgb(255, 1, 65),
+
+            Color.FromRgb(254, 0, 0),
+            Color.FromRgb(128, 255, 0),
+            Color.FromRgb(1, 255, 255),
+            Color.FromRgb(127, 0, 255),
+
+            Color.FromRgb(255, 64, 1),
+            Color.FromRgb(64, 255, 1),
+            Color.FromRgb(0, 192, 255),
+            Color.FromRgb(191, 0, 254),
+
+            Color.FromRgb(255, 127, 0),
+            Color.FromRgb(0, 255, 1),
+            Color.FromRgb(0, 128, 255),
+            Color.FromRgb(255, 0, 254),
+
+            Color.FromRgb(255, 192, 0),
+            Color.FromRgb(0, 255, 65),
+            Color.FromRgb(0, 65, 255),
+            Color.FromRgb(255, 0, 192),
+
+            Color.FromRgb(255, 255, 1),
+            Color.FromRgb(0, 254, 129),
+            Color.FromRgb(0, 0, 254),
+            Color.FromRgb(255, 0, 128),
+
+            Color.FromRgb(192, 255, 0),
+            Color.FromRgb(1, 255, 193),
+            Color.FromRgb(63, 0, 255),
+            Color.FromRgb(255, 1, 65),
+
+            Color.FromRgb(254, 0, 0),
+            Color.FromRgb(128, 255, 0),
+            Color.FromRgb(1, 255, 255),
+            Color.FromRgb(127, 0, 255),
+
+            Color.FromRgb(255, 64, 1),
+            Color.FromRgb(64, 255, 1),
+            Color.FromRgb(0, 192, 255),
+            Color.FromRgb(191, 0, 254),
+
+            Color.FromRgb(255, 127, 0),
+            Color.FromRgb(0, 255, 1),
+            Color.FromRgb(0, 128, 255),
+            Color.FromRgb(255, 0, 254),
+
+            Color.FromRgb(255, 192, 0),
+            Color.FromRgb(0, 255, 65),
+            Color.FromRgb(0, 65, 255),
+            Color.FromRgb(255, 0, 192),
+
+            Color.FromRgb(255, 255, 1),
+            Color.FromRgb(0, 254, 129),
+            Color.FromRgb(0, 0, 254),
+            Color.FromRgb(255, 0, 128),
+
+            Color.FromRgb(192, 255, 0),
+            Color.FromRgb(1, 255, 193),
+            Color.FromRgb(63, 0, 255),
+            Color.FromRgb(255, 1, 65),
+
+            Color.FromRgb(254, 0, 0),
+            Color.FromRgb(128, 255, 0),
+            Color.FromRgb(1, 255, 255),
+            Color.FromRgb(127, 0, 255),
+
+            Color.FromRgb(255, 64, 1),
+            Color.FromRgb(64, 255, 1),
+            Color.FromRgb(0, 192, 255),
+            Color.FromRgb(191, 0, 254),
+
+            Color.FromRgb(255, 127, 0),
+            Color.FromRgb(0, 255, 1),
+            Color.FromRgb(0, 128, 255),
+            Color.FromRgb(255, 0, 254),
+
+            Color.FromRgb(255, 192, 0),
+            Color.FromRgb(0, 255, 65),
+            Color.FromRgb(0, 65, 255),
+            Color.FromRgb(255, 0, 192),
+
+            Color.FromRgb(255, 255, 1),
+            Color.FromRgb(0, 254, 129),
+            Color.FromRgb(0, 0, 254),
+            Color.FromRgb(255, 0, 128),
+
+            Color.FromRgb(192, 255, 0),
+            Color.FromRgb(1, 255, 193),
+            Color.FromRgb(63, 0, 255),
+            Color.FromRgb(255, 1, 65),
+
+            Color.FromRgb(254, 0, 0),
+            Color.FromRgb(128, 255, 0),
+            Color.FromRgb(1, 255, 255),
+            Color.FromRgb(127, 0, 255),
+
+            Color.FromRgb(255, 64, 1),
+            Color.FromRgb(64, 255, 1),
+            Color.FromRgb(0, 192, 255),
+            Color.FromRgb(191, 0, 254),
+
+            Color.FromRgb(255, 127, 0),
+            Color.FromRgb(0, 255, 1),
+            Color.FromRgb(0, 128, 255),
+            Color.FromRgb(255, 0, 254),
+
+            Color.FromRgb(255, 192, 0),
+            Color.FromRgb(0, 255, 65),
+            Color.FromRgb(0, 65, 255),
+            Color.FromRgb(255, 0, 192),
+
+            Color.FromRgb(255, 255, 1),
+            Color.FromRgb(0, 254, 129),
+            Color.FromRgb(0, 0, 254),
+            Color.FromRgb(255, 0, 128),
+
+            Color.FromRgb(192, 255, 0),
+            Color.FromRgb(1, 255, 193),
+            Color.FromRgb(63, 0, 255),
+            Color.FromRgb(255, 1, 65),
+
         };
 
         public static Color ErrorColor => Colors.Red;
